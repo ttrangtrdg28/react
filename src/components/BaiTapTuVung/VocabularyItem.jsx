99a412f7621e03cbc2e956bcf79c53294c246cd4
ttrangtrdg28 <ttrangtrdg28@gmail.com>
2023-08-19 13:43:33 +0700

    commit

--- src/components/BaiTapTuVung/VocabularyItem.jsx
+++ src/components/BaiTapTuVung/VocabularyItem.jsx
@@ -6,22 +6,21 @@ export default class VocabularyItem extends Component {
       <div className="border p-3 mb-2" onDoubleClick={() => console.log(2)}>
         <div className="content">
           <div className="d-flex justify-content-between">
-            <h5 className="font-weight-bold">{this.props.element.en}</h5>(
-            {this.props.element.isFavorited} ? (
+            <h5 className="font-weight-bold">{this.props.element.en}</h5>
+            {this.props.element.isFavorited} ?
             <i
               onClick={() =>
                 this.props.handleChangeFavorite(this.props.element.id, true)
               }
               className="fa-solid fa-heart text-danger"
             />
-            ) :
+            :
             <i
               onClick={() =>
                 this.props.handleChangeFavorite(this.props.element.id, false)
               }
               className="fa-regular fa-heart"
             />
-            )
           </div>
           <p className="mb-0">{this.props.element.vi}</p>
         </div>
